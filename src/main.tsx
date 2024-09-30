99a8cb7af8fbd1ff12e200b90a8c7bb70cc52f3d
zane-x <zane.x@binance.com>
2024-09-30 14:23:38 +0900

    feat: add appId

--- src/main.tsx
+++ src/main.tsx
@@ -1,47 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // Redux
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 // TonConnect UI
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // Rainbow Kit
-import './polyfills';
+import "./polyfills";
 
-import { createWalletConnectModal } from './configs/walletConnect';
+import { createWalletConnectModal } from "./configs/walletConnect";
 
 // Telegram Mini App SDK
-import WebApp from '@twa-dev/sdk';
+import WebApp from "@twa-dev/sdk";
 
 // App + Styles
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
 
+console.log("main");
 // Hide the main button
 WebApp.MainButton.hide();
-// Expand the Telegram Mini App to full screen
+// // Expand the Telegram Mini App to full screen
 WebApp.expand();
-// Initialize the Telegram Mini App SDK
+// // Initialize the Telegram Mini App SDK
 WebApp.ready();
-// Enable the closing confirmation
+// // Enable the closing confirmation
 WebApp.enableClosingConfirmation();
 
-// Create the WalletConnect modal
-createWalletConnectModal();
+// // Create the WalletConnect modal
+try {
+  createWalletConnectModal();
+} catch (e) {
+  console.log(e);
+}
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
+console.log("render");
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <TonConnectUIProvider manifestUrl="https://softstack.github.io/telegram-mini-app/tonconnect-manifest.json">
-                <App />
-            </TonConnectUIProvider>
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <TonConnectUIProvider manifestUrl="https://softstack.github.io/telegram-mini-app/tonconnect-manifest.json">
+        <App />
+      </TonConnectUIProvider>
+    </Provider>
+  </React.StrictMode>
 );
